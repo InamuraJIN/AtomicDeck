ee82fca9539051494b6dbfad6e2b7d100319c790
Inamura JIN <magic.inamura.jin@gmail.com>
2025-03-05 01:02:36 +0900

    01

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,13 @@
-export default {
-    server: {
-      port: 3000
-    }
-  };
-  
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// Vercelのデプロイに適した設定
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: "dist", // 出力ディレクトリ
+  },
+  server: {
+    port: 3000,
+  },
+});
